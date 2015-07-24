a72caf0e717d5ddc6a49aac7368a866f7b39ea65
vitor hugo campos <vitorh45@gmail.com>
2015-07-24 04:38:11 -0300

    Change the database config

--- avaliacao/settings.py
+++ avaliacao/settings.py
@@ -89,23 +89,19 @@ WSGI_APPLICATION = 'avaliacao.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
+#
+# # Enable Connection Pooling
+# DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
-# Enable Connection Pooling
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
 
-
-import dj_database_url
-import os
-if os.getcwd() == "/app":
-    DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Internationalization
