aa09789cabf3ef1b66e73f99c6e0d6f8043e80a0
vitor hugo campos <vitorh45@gmail.com>
2015-07-24 04:46:20 -0300

    Test

--- avaliacao/settings.py
+++ avaliacao/settings.py
@@ -89,19 +89,20 @@ WSGI_APPLICATION = 'avaliacao.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 
-# import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
-#
-# # Enable Connection Pooling
-# DATABASES['default']['ENGINE'] = 'django_postgrespool'
-
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Internationalization
